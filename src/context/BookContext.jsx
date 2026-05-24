1975a09a8900926724b2604a9f9432147b8b56d3
kaanarslann <kaanarslan_hp@hotmail.com>
2026-05-24 15:07:15 +0300

    added BookContext

---
+++ src/context/BookContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useState } from "react";
+import { initialBooks } from "../data/booksData";
+
+export const BookContext = createContext();
+
+export const BookContextProvider = ({children}) => {
+    const [books, setBooks] = useState(initialBooks);
+
+    const addBook = (book) => {
+        setBooks((prev) => [
+            ...prev,
+            {...book, id: Date.now()},
+        ]);
+    };
+
+    const updateBook = (updatedBook) => {
+        setBooks((prev) => prev.map((book) => book.id === updateBook.id ? updateBook : book));
+    };
+
+    const deleteBook = (id) => {
+        setBooks((prev) => prev.filter((book) => book.id !== id));
+    };
+
+    return (
+        <BookContext.Provider value={{books, addBook, updateBook, deleteBook}}>
+            {children}
+        </BookContext.Provider>
+    );
+}
